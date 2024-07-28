4921bfd192fef77e28577b290f4518f39dab4dc2
kron825 <gpjmjjmpnsvq159@gmail.com>
2024-07-28 23:12:22 +0900

    Update 計算式.java

--- 計算式.java
+++ 計算式.java
@@ -3,11 +3,13 @@ public class 計算式//←ここにファイル名を
 {
     public void main(String args[]){
         Scanner scan=new Scanner(System.in);
-        int k,i,time;
+        int k,i,ni;
         double s;
         System.out.print("");
         System.out.print("日数を入力");
-        time=scan.nextInt;
-        
+        ni=scan.nextInt;
+        s=ni*24+6;
+        System.out.print("s+"時間");
+        scan.close();
     }
 }
